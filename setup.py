d052834b25eff96a010a02261394f265701090c5
Lukas Meyer <lukas.meyer@fau.de>
2022-09-15 16:36:18 +0200

    changed version and conda env

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import setuptools
 
 setuptools.setup(
     name='colmap_wrapper',
-    version='1.1.0',
+    version='1.1.1',
     description='COLMAP Handler',
     license="MIT",
     long_description='',
