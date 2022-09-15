cb6b3635d8532006e86e4e45f602dc669e628523
Lukas Meyer <lukas.meyer@fau.de>
2022-09-15 14:10:34 +0200

    version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import setuptools
 
 setuptools.setup(
     name='colmap_wrapper',
-    version='0.0.1',
+    version='1.1.0',
     description='COLMAP Handler',
     license="MIT",
     long_description='',
