a510626f5b03a6abf72b79ec3e4aa585eac8110b
Lukas Meyer <lukas.meyer@fau.de>
2023-05-31 11:09:08 +0200

    fixed test.py

--- colmap_wrapper/__init__.py
+++ colmap_wrapper/__init__.py
@@ -5,6 +5,7 @@ Copyright (c) 2022 Lukas Meyer
 Licensed under the MIT License.
 See LICENSE file for more information.
 """
+import os
 
 from colmap_wrapper.dataloader import *
 from colmap_wrapper.reconstruction import *
@@ -12,6 +13,7 @@ from colmap_wrapper.visualization import *
 from colmap_wrapper.data import *
 from colmap_wrapper.gps import *
 
-import getpass
+if not os.getenv("GITHUB_ACTIONS"):
+    import getpass
 
-USER_NAME = getpass.getuser()
+    USER_NAME = getpass.getuser()
